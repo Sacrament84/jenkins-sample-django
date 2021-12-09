260302afd1d7c1ae565f6ea3186863e07d668a9e
root <voloshin07@gmail.com>
2021-12-09 11:59:35 +0600

    change db to sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,6 +90,13 @@ if os.getenv("DEVELOPMENT_MODE", "False") == "True":
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+
 #    DATABASES = {
 #        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 #    }
